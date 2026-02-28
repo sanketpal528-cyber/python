3e522079e4de03bc7625b84115adcf6a798b220f
sanketpal528 <sanketpal528@gmail.com>
2026-03-01 02:40:47 +0530

    operators

---
+++ 28.02.26/04_operators.py
@@ -0,0 +1,36 @@
+# airthmetic operators
+a = 10 
+b = 25
+c = a+b
+print (c) 
+
+# assingment opertaors 
+a = 4-2 # asign 4-2 in a
+print (a)
+b = 6 
+b += 3 # increment in the value of b by 3 and assign it to b
+print (b)
+
+# comparison perators
+d = 6!=8
+print (d)
+
+# logical operators 
+e  = 5>3 and 6>4
+print (e)
+
+# truth table for or oprators
+print ("true or false =", True or False)
+print ("false or false is", False or  False)
+print ("true or true is", True or True)
+print ("false or true is", False or True)
+
+# truth table for and oprators
+print ("true and false =", True and False)
+print ("false and false is", False and  False)
+print ("true and true is", True and True)
+print ("false and true is", False and True)
+
+# truth table for not operator
+print ("not true is", not True)
+print ("not false is", not False)
--- 3.py
+++ 3.py
@@ -1,4 +1,4 @@
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("hello sanket pal, how are you doing?")
+engine.say("hello dev, how are you doing?")
 engine.runAndWait()
