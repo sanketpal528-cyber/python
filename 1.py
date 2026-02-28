c950acc12dfdb16c615ed1b695866136ef3c59eb
sanketpal528 <sanketpal528@gmail.com>
2026-02-28 14:06:22 +0530

    eg.3

--- 1.py
+++ 1.py
@@ -1,2 +1,15 @@
-for i in range(1, 11):
-    print(5 * i)
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+Twinkle, twinkle, little star,
+How I wonder what you are!
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Twinkle, twinkle, little star,
+How I wonder what you are! ''')
