bee7efee3773facf5fc30135a4e11571e3395c31
sanketpal528 <sanketpal528@gmail.com>
2026-02-28 17:49:16 +0530

    datatype.py

---
+++ 28.02.26/02_datatype.py
@@ -0,0 +1,5 @@
+a = 1 # a is a integer variable
+b = 7.23 # b is a float variable 
+c = "sanket pal" # c is a string variable
+d = True # d is a boolean variable
+e = None # e is a none type variable
---
+++ 28.02.26/03_rules_variables.py
@@ -0,0 +1,13 @@
+# RULES FOR CHOOSING AN IDENTIFIER
+
+#  A variable name can contain alphabets, digits, and underscores.
+# • A variable name can only start with an alphabet and underscores.
+# • A variable name can’t start with a digit.
+# • No while space is allowed to be used inside a variable name
+
+aa = 42
+san_ket = "sanket pal"
+hhsdjajd = 7.233
+_aryan = 63
+sanket@528 = 63 # invalid variable name because it contains @ symbol
+
