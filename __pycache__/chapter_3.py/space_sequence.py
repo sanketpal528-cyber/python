1e18ab55b842b0df20e0dd94c94f1ff8cb7cb642
sanketpal528 <sanketpal528@gmail.com>
2026-03-28 01:54:46 +0530

    chapter 3

---
+++ __pycache__/chapter_3.py/space_sequence.py
@@ -0,0 +1,17 @@
+a = "my name is sanket pal\n i am a software developer"
+print(a)
+
+b = "my name is sanket pal\t i am a software developer"
+print(b)
+
+c = "my name is sanket pal\r i am a software developer"
+print(c)
+
+d = "my name is sanket pal\f i am a software developer"
+print(d)
+
+e = "my name is sanket pal\v i am a software developer"
+print(e)
+
+f = "my name is sanket pal\n i am a \"software developer\""
+print(f)
